f99b97781d776b62bda1e2f3ba6e205a558ceafd
Ruan Rita <54330181+Ruan-Rita@users.noreply.github.com>
2025-01-12 12:00:06 -0300

    Infra: add database and structure basic

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { LibraryModule } from './library/library.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { RecordModule } from './record/record.module';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [LibraryModule, UserModule, AuthModule, ConfigModule.forRoot(), RecordModule],
+  providers: [],
 })
 export class AppModule {}
